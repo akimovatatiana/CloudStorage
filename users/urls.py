ea3f3add40da11b49f4b546fdf7a377d213b5c3d
Aevum Sonne <aevumsonne@gmail.com>
2020-10-18 21:09:40 +0300

    Project structure changes, copy subscriptions templates to project template folder

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,11 @@ from django.contrib.auth import views
 from django.urls import path
 
 from .views import *
-# from . import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('signup/', signup, name='signup'),
     path('profile/', redirect_user, name="profile"),
-    path('all/', get_all_users, name="user_detail"),
+    path('all/', get_all_users, name="all_users"),
+    path('signup/', signup, name='signup'),
 ]
