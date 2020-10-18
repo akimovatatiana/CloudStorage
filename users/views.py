10a346f1677d25386ea7b24500a0af96136a8d0e
slowsol <slowwsol@gmail.com>
2020-10-18 17:48:34 +0300

    Add subscriptions, add header

--- users/views.py
+++ users/views.py
@@ -2,16 +2,25 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LogoutView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django import forms
 
+
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class UserLogoutView(LogoutView):
+    success_url = reverse_lazy('login')
+    next_page = 'success'
+    template_name = 'registration/logged_out.html'
+
 
 class UserSignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
@@ -20,7 +29,8 @@ class UserSignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
+
 
 def signup(request):
     if request.method == 'POST':
@@ -35,6 +45,7 @@ def signup(request):
     else:
         form = UserSignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
 
 @login_required
 def redirect_user(request):
