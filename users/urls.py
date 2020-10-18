ebbbab4cf454cb74c60c3c172ce28575e480f65b
akimovatatiana <akimova.tnya@gmail.com>
2020-10-18 12:23:28 +0300

    Redirect after signup to profile

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,8 @@ from .views import *
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('signup/', SignUpView.as_view(), name='signup'),
+    #path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup/', signup, name='signup'),
     path('profile/', redirect_user, name="profile"),
     path('all/', get_all_users, name="user_detail"),
 ]
