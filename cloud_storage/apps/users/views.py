8c1a022a643d7ac035f3effddd039455de6d9b9f
slowsol <slowwsol@gmail.com>
2020-12-24 10:21:41 +0300

    Signup view now class based

--- cloud_storage/apps/users/views.py
+++ cloud_storage/apps/users/views.py
@@ -1,3 +1,5 @@
+from django.views import View
+
 from .forms import *
 
 from django.contrib.auth import login, authenticate
@@ -6,68 +8,38 @@ from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from django.shortcuts import render, redirect, reverse
 
-from subscriptions.models import UserSubscription
+from subscriptions.models import UserSubscription, SubscriptionPlan
 from subscriptions import views as sub_views
 
 
-def get_user(request, pk):
-    user = User.objects.get(pk=pk)
-    user_subscription = UserSubscription.objects.get_queryset()
-
-    if user_subscription.filter(user=user):
-        user_plan = user_subscription.filter(user=user)[0].subscription.plan
-    else:
-        user_plan = None
-
-    context = {
-        "user": user,
-        "plan": user_plan
-    }
-
-    return render(request, "profile/profile.html", context)
-
-
-def get_all_users(request):
-    keys = User.objects.count() + 1
-    users = []
-
-    # Traverse all users by primary keys
-    for pk in range(1, keys):
-        user = User.objects.get(pk=pk)
-        users.append(user)
-
-    context = {
-        "users": users
-    }
-
-    return render(request, "profile/users.html", context)
-
-
-@login_required
-def redirect_user(request):
-    return get_user(request, pk=request.user.pk)
-
-
-def signup(request):
-    plan_id = request.POST.get('plan_id', '')
-    redirect_from = request.POST.get('redirect_from', '')
-
-    if request.method == 'POST' and plan_id != '' and redirect_from == 'signup':
-        form = UserSignUpForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-
-            login(request, user)
-
-            return sub_views.SubscribeView.as_view()(request)
-    else:
+class SignUpView(View):
+    def get(self, request):
         form = UserSignUpForm()
+        plan_id = request.POST.get('plan_id', '')
 
-    return render(request, 'registration/signup.html', {'form': form, 'plan_id': plan_id})
+        return render(self.request, 'registration/signup.html', {'form': form, 'plan_id': plan_id})
+
+    def post(self, request):
+        form = UserSignUpForm()
+        plan_id = request.POST.get('plan_id', '')
+
+        plan = SubscriptionPlan.objects.filter(pk=plan_id)
+        redirect_from = self.request.POST.get('redirect_from', '')
+
+        if plan and redirect_from == 'signup':
+            form = UserSignUpForm(request.POST)
+
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+
+                login(request, user)
+
+                return sub_views.SubscribeView.as_view()(request)
+        else:
+            return render(self.request, 'registration/signup.html', {'form': form, 'plan_id': plan_id})
 
 
 @login_required
