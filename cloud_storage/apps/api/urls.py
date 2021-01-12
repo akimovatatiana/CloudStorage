1459797ab53b578eb9d3e5ed2edd0b01addc068b
slowsol <slowwsol@gmail.com>
2021-01-12 11:59:06 +0300

    Remove get_all_users()

--- cloud_storage/apps/api/urls.py
+++ cloud_storage/apps/api/urls.py
@@ -1,9 +1,7 @@
-from django.contrib.auth import views
 from django.conf.urls import url
 
 from .views import *
 
 urlpatterns = [
     url('used-size', get_used_size_json, name='used-size'),
-    url('users/all/', get_all_users, name="all_users"),
 ]
